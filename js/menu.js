cde884daee9a4054604506bec488e342b3effe0e
Lynn Nijboer <lynnnijboer12@gmail.com>
2022-12-12 09:17:55 +0100

    first commit

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+// const hamburgerBtn = document.querySelector('.menu');
+// const navMenu = document.querySelector('.mobile-menu-items');
+
+// hamburgerBtn.addEventListener('click', e => {
+//     navMenu.classList.toggle('active');
+// });
+
+// const faqItem = document.querySelector('.faq-item');
+// const faqAnswer = document.querySelector('.faq-item-answer');
+
+// faqItem.addEventListener('click', e => {
+//     faqAnswer.classList.toggle('active');
+// });
+
+// console.log(faqAnswer)
